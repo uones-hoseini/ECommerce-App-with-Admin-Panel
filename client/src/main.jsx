e85af044b3d08a03f8e26dedb9ffcec873068033
Seyed Younes Hosseini <73585746+uones-hoseini@users.noreply.github.com>
2024-12-29 23:18:37 +0330

    add reducer toolkid basic and slice

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,14 @@
+import { createRoot } from "react-dom/client"
+import "./index.css"
+import App from "./App.jsx"
+import { BrowserRouter } from "react-router-dom"
+import { Provider } from "react-redux"
+import store from "./store/store"
 
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-
-createRoot(document.getElementById('root')).render(
-<BrowserRouter>
-<App />
-
-</BrowserRouter>
-
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
 )
