944513fe808b62ba69c45e1668fc76b974512793
Seyed Younes Hosseini <73585746+uones-hoseini@users.noreply.github.com>
2024-12-30 16:36:12 +0330

    login form is ended

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -21,4 +21,19 @@ export const registerFormControls = [
 		type: "password",
 	},
 ]
-
+export const loginFormControls = [
+	{
+		name: "email",
+		lable: "Email",
+		placeholder: "Enter your email",
+		componentType: "input",
+		type: "email",
+	},
+	{
+		name: "password",
+		lable: "Password",
+		placeholder: "Enter your password",
+		componentType: "input",
+		type: "password",
+	},
+]
